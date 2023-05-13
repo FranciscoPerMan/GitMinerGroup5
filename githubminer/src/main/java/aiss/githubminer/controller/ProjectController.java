815365aeb1b84b79a0606087191a3db9d31c78fc
FranciscoPerezUS <fraperman2@alum.us.es>
2023-05-13 19:33:27 +0200

    Controller class and renaming of TransformerService

---
+++ githubminer/src/main/java/aiss/githubminer/controller/ProjectController.java
@@ -0,0 +1,43 @@
+package aiss.githubminer.controller;
+
+import aiss.githubminer.model.gitminer.GMProject;
+import aiss.githubminer.service.GitHubService;
+import aiss.githubminer.service.GitMiner;
+import aiss.githubminer.service.TransformerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/github")
+public class ProjectController {
+
+
+    @Autowired
+    private final GitHubService gitHubService;
+
+    @Autowired
+    private final GitMiner gitMinerService;
+    @Autowired
+    private final TransformerService transformerService;
+    public ProjectController(GitHubService gitHubService, GitMiner gitMinerService, TransformerService transformerService) {
+        this.gitHubService = gitHubService;
+        this.gitMinerService = gitMinerService;
+        this.transformerService = transformerService;
+    }
+    @GetMapping("/{owner}/{repository}")
+    public GMProject getProjects(@PathVariable String owner,@PathVariable String repository, @RequestParam(defaultValue = "2" ) Integer sinceCommits,
+                                 @RequestParam(defaultValue="20") Integer sinceIssues, @RequestParam(defaultValue="2") Integer maxPages){
+        System.out.println("Owner: " + owner+ " Repository: " + repository + " sinceCommits: " + sinceCommits + " sinceIssues: " + sinceIssues + " maxPages: " + maxPages);
+        return transformerService.getCompleteGMProject(owner,repository, sinceCommits, sinceIssues, maxPages);
+    }
+
+    @PostMapping("/{owner}/{repository}")
+    public GMProject postProjects(@PathVariable String owner,@PathVariable String repository, @RequestParam(defaultValue = "2" ) Integer sinceCommits,
+                                  @RequestParam(defaultValue="20") Integer sinceIssues, @RequestParam(defaultValue="2") Integer maxPages){
+
+        GMProject transformed =  transformerService.getCompleteGMProject(owner,repository, sinceCommits, sinceIssues, maxPages);
+        return gitMinerService.postProject(transformed);
+    }
+
+
+}
--- githubminer/src/main/java/aiss/githubminer/service/TransformerService.java
+++ githubminer/src/main/java/aiss/githubminer/service/TransformerService.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TranformerService {
+public class TransformerService {
     @Autowired
     GitHubService service;
 
