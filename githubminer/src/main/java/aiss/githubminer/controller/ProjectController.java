ae0394d5afc736cecf3f93035650e81e49c84dab
DXD-Tech <diegopeque9@gmail.com>
2023-05-14 23:02:53 +0200

    Add automatic documentation of GitLabMiner and GitHubMiner

--- githubminer/src/main/java/aiss/githubminer/controller/ProjectController.java
+++ githubminer/src/main/java/aiss/githubminer/controller/ProjectController.java
@@ -1,16 +1,23 @@
 package aiss.githubminer.controller;
 
 import aiss.githubminer.model.gitminer.GMProject;
+import aiss.githubminer.model.projects.Project;
 import aiss.githubminer.service.GitHubService;
 import aiss.githubminer.service.GitMiner;
 import aiss.githubminer.service.TransformerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Project", description = "GitMiner Project controller for GitHub")
 @RestController()
 @RequestMapping("/github")
 public class ProjectController {
-
 
     @Autowired
     private final GitHubService gitHubService;
@@ -24,6 +31,15 @@ public class ProjectController {
         this.gitMinerService = gitMinerService;
         this.transformerService = transformerService;
     }
+
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Project created", content = @Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Project was not found on GitHub", content = @Content(schema = @Schema(), mediaType = "application/json"))
+    })
+    @Operation(summary = "Get project",
+            description = "Get a GitHub project in the format of a GitMiner project. You can limit the number of issues " +
+                    "and commits you obtain from the repository with the sinceCommits, sinceIssues and maxPages parameters.",
+            tags = {"Projects"})
     @GetMapping("/{owner}/{repository}")
     public GMProject getProjects(@PathVariable String owner,@PathVariable String repository, @RequestParam(defaultValue = "2" ) Integer sinceCommits,
                                  @RequestParam(defaultValue="20") Integer sinceIssues, @RequestParam(defaultValue="2") Integer maxPages){
@@ -31,6 +47,14 @@ public class ProjectController {
         return transformerService.getCompleteGMProject(owner,repository, sinceCommits, sinceIssues, maxPages);
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Project created", content = @Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Project was not found on GitHub", content = @Content(schema = @Schema(), mediaType = "application/json"))
+    })
+    @Operation(summary = "Create a new project in GitMiner",
+            description = "Take the data of an existing GitHub project to create a new project in the GitMiner database. You can limit the number of issues " +
+                    "and commits you obtain from the repository with the sinceCommits, sinceIssues and maxPages parameters.\"",
+            tags = {"Projects"})
     @PostMapping("/{owner}/{repository}")
     public GMProject postProjects(@PathVariable String owner,@PathVariable String repository, @RequestParam(defaultValue = "2" ) Integer sinceCommits,
                                   @RequestParam(defaultValue="20") Integer sinceIssues, @RequestParam(defaultValue="2") Integer maxPages){
--- gitlabminer/src/main/java/aiss/gitlabminer/controller/ProjectController.java
+++ gitlabminer/src/main/java/aiss/gitlabminer/controller/ProjectController.java
@@ -1,15 +1,23 @@
 package aiss.gitlabminer.controller;
 
+import aiss.gitlabminer.model.gitlab.Project;
 import aiss.gitlabminer.model.gitminer.GMProject;
 import aiss.gitlabminer.service.GitLabService;
 import aiss.gitlabminer.service.GitMinerService;
 import aiss.gitlabminer.service.TransformerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Optional;
 
+@Tag(name = "Project", description = "GitMiner Project controller for GitLab")
 @RestController()
 @RequestMapping("/gitlab")
 public class ProjectController {
@@ -26,12 +34,28 @@ public class ProjectController {
         this.gitMinerService = gitMinerService;
         this.transformerService = transformerService;
     }
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Project created", content = @Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Project was not found on GitLab", content = @Content(schema = @Schema(), mediaType = "application/json"))
+    })
+    @Operation(summary = "Get project",
+            description = "Get a GitLab project in the format of a GitMiner project. You can limit the number of issues " +
+                    "and commits you obtain from the repository with the sinceCommits, sinceIssues and maxPages parameters.",
+            tags = {"Projects"})
     @GetMapping("/{id}")
     public GMProject getProjects(@PathVariable String id, @RequestParam(defaultValue = "2" ) Integer sinceCommits,
                                  @RequestParam(defaultValue="20") Integer sinceIssues, @RequestParam(defaultValue="2") Integer maxPages){
         return transformerService.getCompleteGMProject(id, sinceCommits, sinceIssues, maxPages);
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Project created", content = @Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Project was not found on GitLab", content = @Content(schema = @Schema(), mediaType = "application/json"))
+    })
+    @Operation(summary = "Create a new project in GitMiner",
+            description = "Take the data of an existing GitLab project to create a new project in the GitMiner database. You can limit the number of issues " +
+                    "and commits you obtain from the repository with the sinceCommits, sinceIssues and maxPages parameters.",
+            tags = {"Projects"})
     @PostMapping("/{id}")
     public GMProject postProjects(@PathVariable String id, @RequestParam(defaultValue = "2" ) Integer sinceCommits,
                                   @RequestParam(defaultValue="20") Integer sinceIssues, @RequestParam(defaultValue="2") Integer maxPages){
