69fe65c6ffc9c148931710cb2a3bfc8d36f992b0
David <davgonmar2@alum.us.es>
2023-05-14 12:52:41 +0200

    Fix githubminer->gitminer incorrect uri

--- githubminer/src/main/java/aiss/githubminer/service/GitMiner.java
+++ githubminer/src/main/java/aiss/githubminer/service/GitMiner.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GitMiner {
-    private String gitMinerURI = "http://localhost:8080/api/projects";
+    private String gitMinerURI = "http://localhost:8080/gitminer/projects";
     private RestTemplate restTemplate = new RestTemplate();
 
     public GMProject postProject(GMProject gmProject) {
